22806f6cb6fa0fa9889d856a6327ecd4630d1183
Aadarsh Nagrath <anagrath1@gmail.com>
2024-04-27 14:18:04 +0530

    skin cancer issue fixed

--- pages/Skin Cancer Prediction.py
+++ pages/Skin Cancer Prediction.py
@@ -1,15 +1,12 @@
 import streamlit as st
 
-
 # Function to generate iframe
 def generate_iframe(url):
-    iframe = f'<iframe src="{url}" width="1000" height="1600" scrolling="no" ;"></iframe>'
+    iframe = f'<iframe src="{url}" width="1000" height="1600" scrolling="no"></iframe>'
     return iframe
-
 
 # Main Streamlit app
 def main():
-
     # Get the URL from the user
     website_url = "https://aadarsh-nagrath.github.io/sc/"
 
@@ -17,6 +14,5 @@ def main():
         # Generate and display the iframe
         st.write(generate_iframe(website_url), unsafe_allow_html=True)
 
-
-if _name_ == "_main_":
-    main()
+if __name__ == "__main__":
+    main()
